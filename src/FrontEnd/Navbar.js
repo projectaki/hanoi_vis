7e5b96289b03a7bd36480e12030858b85a3d83e1
projectaki <akosegypro@gmail.com>
2021-02-15 13:39:58 +0100

    back button

--- src/FrontEnd/Navbar.js
+++ src/FrontEnd/Navbar.js
@@ -5,8 +5,25 @@ class Navbar extends Component {
         return (
             <div>
                 <nav>
-                    <div class="nav-wrapper" style={{textAlign: "center"}}>
-                        <p style={{fontSize: "3vh"}}>Towers of Hanoi</p>
+                    <div class="nav-wrapper" >
+                    <div className="navgrid" style={{display: "grid", gridTemplateColumns: "20% 80%"}}>
+                        <div classname="navgrid-item" style={{paddingLeft: "2vh", marginTop: 0, paddingTop: 0}}>
+                            <a href="https://projectaki.github.io/portfolio_akos_madarasz/#/Projects"  >
+                            <i class="fas fa-arrow-left"></i>
+                            </a>
+                        </div>
+                        <div classname="navgrid-item">
+                            <div style={{paddingLeft: "30%"}}>
+                                <p style={{fontSize: "3vh"}}>Towers of Hanoi</p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    
+                    
+                        
+                        
+                        
                     </div>
                 </nav>
             </div>
