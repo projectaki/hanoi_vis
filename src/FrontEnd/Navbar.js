c88a6c673a259839865813a558e862116aa0a3f7
projectaki <akosegypro@gmail.com>
2021-03-02 09:11:25 +0100

    responsiveness
    UI changes

--- src/FrontEnd/Navbar.js
+++ src/FrontEnd/Navbar.js
@@ -3,29 +3,8 @@ import React, { Component } from 'react';
 class Navbar extends Component {
     render() {
         return (
-            <div>
-                <nav>
-                    <div class="nav-wrapper" >
-                    <div className="navgrid" style={{display: "grid", gridTemplateColumns: "20% 80%"}}>
-                        <div classname="navgrid-item" style={{paddingLeft: "2vh", marginTop: 0, paddingTop: 0}}>
-                            <a href="https://projectaki.github.io/portfolio_akos_madarasz/#/Projects"  >
-                            <i class="fas fa-arrow-left"></i>
-                            </a>
-                        </div>
-                        <div classname="navgrid-item">
-                            <div style={{paddingLeft: "30%"}}>
-                                <p style={{fontSize: "3vh"}}>Towers of Hanoi</p>
-                            </div>
-                        </div>
-                    </div>
-                    
-                    
-                    
-                        
-                        
-                        
-                    </div>
-                </nav>
+            <div className="navbar">
+                <h1 style={{margin: 0, fontSize: "3vmin", paddingTop: "2vmin", paddingBottom: "2vmin", fontFamily: "League Spartan"}}>Towers of Hanoi</h1>
             </div>
         );
     }
--- src/FrontEnd/hanoi.jsx
+++ src/FrontEnd/hanoi.jsx
@@ -13,12 +13,18 @@ export default class Hanoi extends React.Component {
             terminate : true,
             running: false,
             input: 3,
-            setup: false
+            setup: true
             
         };
     }
 
     componentDidMount() {
+        const M = window.M;
+        var elems = document.querySelectorAll('.modal');
+        var instances = M.Modal.init(elems, []);
+        var elem = document.getElementById("modal1");
+        var instance = M.Modal.getInstance(elem);
+        instance.open();
         this.refresh();
         this.setUpDisks();
     }
@@ -54,7 +60,7 @@ export default class Hanoi extends React.Component {
         const elems = document.getElementsByClassName("emptyblock");
         for(let i = 0; i < elems.length; i++) {
             
-            elems[i].style.backgroundColor = "black";
+            elems[i].style.background = "transparent";
             elems[i].style.margin = "auto";
             elems[i].style.boxShadow = "0 0 0 0px black";
         }
@@ -72,7 +78,7 @@ export default class Hanoi extends React.Component {
             
             elems[i].style.backgroundColor = "brown";
             elems[i].style.margin = "auto";
-            elems[i].style.boxShadow = "0 0 0 0.1vw black";
+            elems[i].style.boxShadow = "0 0 0 0.3vmin black";
         }
     }
 
@@ -140,20 +146,29 @@ export default class Hanoi extends React.Component {
             
             <>
                 <div>
+                    
+                <div className="back-btn" style={{position: "absolute", padding: 0, margin: 0}}>
+                    <a href="https://projectaki.github.io/portfolio_akos_madarasz/#/Projects" style={{color: "black"}} >
+                        <i class="fas fa-arrow-left fa-2x"></i>
+                    </a>
+                </div>
                     <div className="container" style={{textAlign: "center"}}>
-                    <label htmlFor="diskRange" class="form-label" style={{fontSize: "2vw"}}>Number of discs</label>
+                    <label htmlFor="diskRange" class="form-label" style={{fontSize: "2vmin", color: "white", fontFamily: "Alegreya sans"}}>Number of discs</label>
                     <input value={this.state.input} onChange={evt => this.updateInput(evt)} type="range" class="form-range" min="1" max="8" step="1" id="diskRange"
-                    style={{height: "3vh"}}
+                    
                     />
-                    <div className="white-text" style={{fontSize: "2vw"}}>
+                    <div className="white-text" style={{fontSize: "2vw", fontFamily: "League Spartan"}}>
                         {this.state.input}
                     </div>
+                    <button onClick={() => this.solve()}>SOLVE</button>
+                    <button onClick={() => this.reset()}>RESET</button>       
                     </div>
                     <div className="tcontainer">
                         <div class="row" style={{paddingTop: "10vh"}}>
 
                             <div class="col s4">
-                                <div className="towercont">
+                                <div className="towercont" >
+                                <div style={{position: "absolute",left:"16.4%", zIndex: "-1", padding: 0, margin: 0, backgroundColor: "white", height: `${tower1.length * 2 + 4}vw`, width: "1vmin", marginTop: "-4vw"}}></div>
                                     {tower1.map((block, id) => {
                                         return <div key={id} className="emptyblock" style={{width: `${block }vw`, height: `2vw`}}></div>
                                     })}
@@ -161,6 +176,7 @@ export default class Hanoi extends React.Component {
                             </div>
                             <div class="col s4">
                                 <div className="towercont">
+                                <div style={{position: "absolute",left:"49.7%", zIndex: "-1", padding: 0, margin: 0, backgroundColor: "white", height: `${tower1.length * 2 + 4}vw`, width: "1vmin", marginTop: "-4vw"}}></div>
                                     {tower1.map((block, id) => {
                                         return <div key={id} className="emptyblock" style={{width: `${block}vw`, height: `2vw`}}></div>
                                     })}
@@ -168,6 +184,7 @@ export default class Hanoi extends React.Component {
                             </div>
                             <div class="col s4">
                                 <div className="towercont">
+                                <div style={{position: "absolute",left:"83%", zIndex: "-1", padding: 0, margin: 0, backgroundColor: "white", height: `${tower1.length * 2 + 4}vw`, width: "1vmin", marginTop: "-4vw"}}></div>
                                     {tower1.map((block, id) => {
                                         return <div key={id} className="emptyblock" style={{width: `${block}vw`, height: `2vw`}}></div>
                                     })}
@@ -179,46 +196,21 @@ export default class Hanoi extends React.Component {
                         
                         
                     </div>
-                    <div className="container" style={{marginTop: "3vh"}}>
-                    <div class="grid-container">
-                            <div className="grid-item">
-                                
-                                <button className="waves-effect waves-light btn" onClick={() => this.solve()}>
-                                    <label className="white-text" style={{fontSize: "1vw"}}>solve</label>
-                                </button>
-                            </div>
-                            <div className="grid-item">
-                                <button className="waves-effect waves-light btn" onClick={() => this.reset()}>
-                                    <label className="white-text" style={{fontSize: "1vw"}}>reset</label>
-                                </button>
-                            </div>
 
-                    </div>
-                        
+                    <div id="modal1" class="modal">
+                        <div class="modal-content">
+                        <h4 class="resp-head">Towers of Hanoi Puzzle Description</h4>
+                        <p style={{fontFamily: "Alegreya sans", fontSize: "2vmin"}}>The Tower of Hanoi (also called the Tower of Brahma or Lucas' Tower[1] and sometimes pluralized as Towers) is a mathematical game or puzzle. It consists of three rods and a number of disks of different sizes, which can slide onto any rod. The puzzle starts with the disks in a neat stack in ascending order of size on one rod, the smallest at the top, thus making a conical shape.
+
+                    The objective of the puzzle is to move the entire stack to another rod, obeying the following simple rules:
+
+                    Only one disk can be moved at a time.
+                    Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack or on an empty rod.
+                    No larger disk may be placed on top of a smaller disk. <a href="https://en.wikipedia.org/wiki/Tower_of_Hanoi" style={{fontSize:"1vw"}}>[source]</a></p>
+                        </div>
                         
                     </div>
-                    <div className="container">
-                    <div class="row">
-                        <div class="col s12 m12">
-                        <div class="card blue-grey ">
-                            <div class="card-content white-text">
-                            <h4 class="resp-head">Towers of Hanoi Puzzle Description</h4>
-                            <p>The Tower of Hanoi (also called the Tower of Brahma or Lucas' Tower[1] and sometimes pluralized as Towers) is a mathematical game or puzzle. It consists of three rods and a number of disks of different sizes, which can slide onto any rod. The puzzle starts with the disks in a neat stack in ascending order of size on one rod, the smallest at the top, thus making a conical shape.
-
-                                The objective of the puzzle is to move the entire stack to another rod, obeying the following simple rules:
-
-                                Only one disk can be moved at a time.
-                                Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack or on an empty rod.
-                                No larger disk may be placed on top of a smaller disk. <a href="https://en.wikipedia.org/wiki/Tower_of_Hanoi" style={{fontSize:"1vw"}}>[source]</a>
-                                
-                                
-                            </p>
-                            </div>
-                            
-                        </div>
-                        </div>
-                    </div>
-                    </div>
+                   
                 </div>
                 
                 
@@ -257,7 +249,7 @@ function shiftDisk(style1, style2) {
         style2.backgroundColor = style1.backgroundColor;
         style2.margin = style1.margin;
         style2.boxShadow = style1.boxShadow;
-        style1.backgroundColor = "black";
+        style1.background = "transparent";
         style1.boxShadow = "0 0 0 0px black";
 }
 
